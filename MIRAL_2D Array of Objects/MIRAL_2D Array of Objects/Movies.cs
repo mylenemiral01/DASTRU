6720e36ed4a7c2642d84f08b3fbe8afe2422bb66
Mylene Miral <mylenemiral01@gmail.com>
2026-01-13 17:08:16 +0800

    Add files via upload
     2D Array of Objects

---
+++ MIRAL_2D Array of Objects/MIRAL_2D Array of Objects/Movies.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_2D_Array_of_Objects
+{
+     class Movies
+    {
+        public string Title { get; set; }
+        public string Writer { get; set; }
+        public string Director { get; set; }
+
+        public Movies(string title, string writer, string director)
+        {
+            Title = title;
+            Writer = writer;
+            Director = director;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Title: \t{Title}");
+            Console.WriteLine($"Writer: {Writer}");
+        }
+    }
+    class Hollywood : Movies
+    {
+        public string Genre { get; set; }
+
+        public Hollywood(string title, string writer, string director, string genre) : base(title, writer, director)
+        {
+            Genre = genre;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("=====Hollywood Movies Details=====");
+            base.DisplayInfo();
+            Console.WriteLine($"Genre: \t{Genre}\n");
+        }
+    }
+    class Filipino : Movies
+    {
+        public int Duration { get; set; }
+
+        public Filipino(string title, string writer, string director, int duration) : base(title, writer, director)
+        {
+            Duration = duration;
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("=====Filipino Movies Details=====");
+            base.DisplayInfo();
+            Console.WriteLine($"Duration: {Duration} minutes\n");
+        }
+    }
+}
---
+++ MIRAL_2D Array of Objects/MIRAL_2D Array of Objects/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_2D_Array_of_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movies[,] Movies = new Movies[2, 2];
+
+            Movies[0,0] = new Hollywood("Avatar", "James Cameron", "James Cameron", "Science Fiction");
+            Movies[0,1] = new Hollywood("Spider-Man", "Stan Lee", "David Cronenberg", "Action");
+
+            Movies[1,0] = new Filipino("The Missing", "Carl Joseph Papa", "Easy Ferrer", 90);
+            Movies[1,1] = new Filipino("One More Chance", "Vanessa R. Valdez and Carmi Raymundo", "Cathy Garcia-Molina", 115);
+
+            for (int i = 0; i < Movies.GetLength(0); i++)
+            {
+                for (int j = 0; j < Movies.GetLength(1); j++)
+                {
+                    Movies[i, j].DisplayInfo();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
