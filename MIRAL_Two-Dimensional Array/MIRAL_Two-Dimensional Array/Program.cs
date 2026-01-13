fe3b45bb7afbe37c71d0542a7af62419ea7eef71
Mylene Miral <mylenemiral01@gmail.com>
2026-01-13 16:54:42 +0800

    Add files via upload
    Two-Dimensional Array

---
+++ MIRAL_Two-Dimensional Array/MIRAL_Two-Dimensional Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIRAL_Two_Dimensional_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize 2D Array
+            string[,] fruits = new string[3, 3]
+            {
+              { "Apple", "Orange", "Banana" },
+              { "Melon", "Avocado", "Grapes" },
+              { "Kiwi", "Pineapple", "Papaya" }
+            };
+              //Display the fruits in the two-dimensional array
+            Console.WriteLine("        Different kind of fruits. \n");
+            Console.WriteLine("     " + "F1"+ "    \t" + "F2" + "   \t\t" + "F3");
+            Console.WriteLine("  -------------------------------------");
+
+            for (int a = 0; a < fruits.GetLength(0); a++)//Iterate through rows
+            {
+                Console.Write("R" + a + "| ");
+                for (int s = 0; s < fruits.GetLength(1); s++)//iterate through colums
+                {
+                    Console.Write(fruits[a, s] + "  \t");//Print each fruit
+                }
+                Console.WriteLine();//New Line for each rows
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MIRAL_Two-Dimensional Array/MIRAL_Two-Dimensional Array/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MIRAL_Two-Dimensional Array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MIRAL_Two-Dimensional Array")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21b7bca9-729f-4b54-82ea-25df14c0ede5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
