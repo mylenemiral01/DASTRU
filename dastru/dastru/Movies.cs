03a8992924744c0b49691650f28cbc17aafbdc46
Mylene Miral <mylenemiral01@gmail.com>
2026-01-22 16:56:10 +0800

    Customize a User-Input Method

---
+++ dastru/dastru/Movies.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dastru
+{
+     class Movies//Base Class
+    { 
+        //Movies Properties
+        public string Title { get; set; } 
+        public string Writer { get; set; }
+        public string Director { get; set; }
+        //Parameterized Constructor
+        public Movies(string title, string writer, string director)
+        {
+            Title = title;
+            Writer = writer;
+            Director = director;
+        }
+        //Virtual method to display information
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Title: \t{Title}");
+            Console.WriteLine($"Writer: {Writer}");
+            Console.WriteLine($"Director: {Director}");
+        }
+    }
+    class Hollywood : Movies//Derived Class
+    {
+        public string Genre { get; set; }//Additional Property
+        //Parameterized Constructor
+        public Hollywood(string title, string writer, string director, string genre) : base(title, writer, director)
+        {
+            Genre = genre;
+        }
+        //Ovveriding method to display information
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("=====Hollywood Movies Details=====");
+            base.DisplayInfo();
+            Console.WriteLine($"Genre: \t{Genre}\n");
+        }
+    }
+    class Filipino : Movies//derived class
+    {
+        public int Duration { get; set; }//Additional Property
+        //Parameterized Constructor
+        public Filipino(string title, string writer, string director, int duration) : base(title, writer, director)
+        {
+            Duration = duration;
+        }
+        //Overriding method to display information
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("=====Filipino Movies Details=====");
+            base.DisplayInfo();
+            Console.WriteLine($"Duration: {Duration} minutes\n");
+        }
+    }
+}
---
+++ dastru/dastru/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dastru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mylene I. Miral
+            //IT401P
+            //ACTIVITY 5: Customize a User-Input Method
+            Movies[,] movie = new Movies[2, 2];
+
+            Console.WriteLine("\tEnter Hollywood movies: ");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"\nHollywood 0.{i + 1} : ");
+                Console.Write("Title: ");
+                string title = Console.ReadLine();
+                Console.Write("Writer: ");
+                string writer = Console.ReadLine();
+                Console.Write("Director: ");
+                string director = Console.ReadLine();
+                Console.Write("Genre: ");
+                string genre = Console.ReadLine();
+
+                movie[0, i] = new Hollywood(title, writer, director, genre);
+            }
+                Console.WriteLine("\n\tEnter Filipino movies: \n");
+                for (int i = 0; i< 2; i++)
+                {
+                    Console.WriteLine($"\nFilipino 0.{i + 1} : ");
+                    Console.Write("Title: ");
+                    string title = Console.ReadLine();
+                    Console.Write("Writer: ");
+                    string writer = Console.ReadLine();
+                    Console.Write("Director: ");
+                    string director = Console.ReadLine();
+                    Console.Write("Duration: ");
+                    int duration = int.Parse (Console.ReadLine());
+
+                    movie[1,i] = new Filipino(title,writer,director,duration);  
+                }
+                
+            Console.Clear();
+            Console.WriteLine("\t LIST OF ALL MOVIES INFORMATION \n");
+            for (int row = 0; row < movie.GetLength(0); row++)
+            {
+                for (int col = 0; col < movie.GetLength(1); col++)
+                {
+                   movie[row, col].DisplayInfo();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
